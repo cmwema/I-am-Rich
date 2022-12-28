dff3e7d536761da68956b609beec6d770a1bc28a
cmwema <calebmwema65@gmail.com>
2022-12-28 13:23:36 +0300

    created app icon for android

--- lib/main.dart
+++ lib/main.dart
@@ -18,25 +18,12 @@ class MyApp extends StatelessWidget {
           title: const Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
-        body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  opacity: 200,
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                      'https://images.unsplash.com/photo-1572304320990-dcf301303d93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGplZXAlMjB3cmFuZ2xlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'))),
-          child: const Center(
-            child: Image(
-              image: NetworkImage(
-                  'https://images.unsplash.com/photo-1600119612651-0db31b3a7baa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZGlhbW9uZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'),
-              width: 200,
-              height: 400,
-            ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
           ),
         ),
-        backgroundColor: Colors.blueGrey[300],
+        backgroundColor: Colors.blueGrey,
       ),
     );
   }
